ed1da36cd40983cd8d3e3ba6793377ced038430e
Kenaz <kenaz@puppetlabs.com>
2020-06-12 21:40:34 -0700

    Adding STS support

--- actions/steps/s3-get-bucket-encryption/step.py
+++ actions/steps/s3-get-bucket-encryption/step.py
@@ -7,9 +7,16 @@ from nebula_sdk import Interface, Dynamic as D
 
 relay = Interface()
 
+session_token = None
+try:
+  session_token = relay.get(D.aws.connection.sessionToken)
+except:
+  pass
+
 sess = boto3.Session(
   aws_access_key_id=relay.get(D.aws.connection.accessKeyID),
-  aws_secret_access_key=relay.get(D.aws.connection.secretAccessKey)
+  aws_secret_access_key=relay.get(D.aws.connection.secretAccessKey),
+  aws_session_token=session_token
 )
 s3 = sess.client('s3')
 
